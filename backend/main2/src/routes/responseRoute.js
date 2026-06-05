c4c0b107d6ca7c20586d315ba9fb76de6a419787
Hardik Bansal <bansalhardik31@gmail.com>
2026-06-05 12:38:53 +0530

    virtual enum and api endpoint

--- backend/main2/src/routes/responseRoute.js
+++ backend/main2/src/routes/responseRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { getAllSurveyResponseBySurveyId, submitSurveyResponse } from "../controllers/responseController.js";
+import { getAllSurveyResponseBySurveyId, submitSurveyResponse, scanAndFetchFlaggedResponses } from "../controllers/responseController.js";
 import { verifyJWT } from "../middleware/verifyJWT.js";
 
 const router = Router();
 router.route("/:survey_id").get(verifyJWT, getAllSurveyResponseBySurveyId).post(submitSurveyResponse);
+router.route("/:survey_id/scan").post(verifyJWT, scanAndFetchFlaggedResponses);
 
 export default router;
